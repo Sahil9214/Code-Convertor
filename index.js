06d91e9792a6ed79c853722c699c07c3ec2034c6
Utkarsh <utkarshsinghal369@gmail.com>
2023-09-14 10:52:13 +0530

    code convertor

--- index.js
+++ index.js
@@ -1,55 +1,113 @@
-const express = require("express");
-const axios = require("axios");
-const app = express();
-const cors = require("cors");
-const { ConversionRouter } = require("./router/conversionRoute");
-require("dotenv").config();
-const PORT = process.env.PORT || 8080;
-const apiKey = process.env.OPENAI_API_KEY;
+const express = require('express')
+const cors = require('cors')
+const dotenv=require('dotenv')
+const axios= require('axios')
+dotenv.config()
 
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-
-app.use(express.json());
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
-
-app.post("/convert", async (req, res) => {
-  try {
-    const { code, language } = req.body;
-
-    const apiUrl =
-      "https://api.openai.com/v1/engines/text-davinci-002/completions";
-
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    };
-
-    const requestData = {
-      prompt: `Convert the following ${language} code:\n\n${code}\n\nTo:\n\n`,
-      max_tokens: 1000,
-    };
-
-    const response = await axios.post(apiUrl, requestData, { headers });
-
-    const convertedCode = response.data.choices[0].text;
-
-    res.status(200).send({ convertedCode });
-  } catch (error) {
-    console.error("Error converting code:", error);
-    res.status(400).json({ error: "Internal server error" });
-  }
-});
+const app= express()
+app.use(express.json())
+app.use(cors())
 
 
-app.listen(PORT, () => {
-  try {
-    console.log(`Server running on port ${PORT}`);
-  } catch (error) {
-    console.log(error);
-  }
-});
+
+app.post('/convert',async(req,res)=>{
+    try {
+        const {code,language}=req.body
+        const prompt=`convert this code ${code} into ${language} only convert the code not write any extra word`
+        const response= await axios.post('https://api.openai.com/v1/chat/completions',{
+            model:'gpt-3.5-turbo',
+            temperature:0.5,
+            max_tokens:2038,
+            messages:[{role:'system',content:'You are a code convertor'},{role:'user',content:prompt}]
+
+        },
+        {
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.API_KEY}`,
+            },
+        }
+    );
+        
+    const codes = response.data.choices[0].message.content;
+    res.json({ codes });
+        
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+app.post('/debug',async(req,res)=>{
+    try {
+        const {code}=req.body
+        const prompt=`debug this code ${code} only debug the code and write some comment if need or not write any extra word`
+        const response= await axios.post('https://api.openai.com/v1/chat/completions',{
+            model:'gpt-3.5-turbo',
+            temperature:0.5,
+            max_tokens:2038,
+            messages:[{role:'system',content:'You are a code debuger'},{role:'user',content:prompt}]
+
+        },
+        {
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.API_KEY}`,
+            },
+        }
+    );
+        
+    const codes = response.data.choices[0].message.content;
+    res.json({ codes });
+        
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+app.post('/checkquality',async(req,res)=>{
+    try {
+        const {code,language}=req.body
+        const prompt=`check quality of this code ${code} and give 3-4 tips and give rating of this code `
+        const response= await axios.post('https://api.openai.com/v1/chat/completions',{
+            model:'gpt-3.5-turbo',
+            temperature:0.5,
+            max_tokens:2038,
+            messages:[{role:'system',content:'You are a code check Quality'},{role:'user',content:prompt}]
+
+        },
+        {
+
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.API_KEY}`,
+            },
+        }
+    );
+        
+    const codes = response.data.choices[0].message.content;
+    res.json({ codes });
+        
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+app.get('/',async(req,res)=>{
+    try {
+        res.status(200).send('Home Page')
+    } catch (error) {
+        res.status(500).send('Some Error')
+    }
+})
+
+
+app.listen(8080,async()=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+    console.log('server running on port 8080 successfully......')
+})
